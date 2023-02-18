5152d08c23d1ca75fd1925f288373b3a547f7581
Haria-27 <kizunayukihma@gmail.com>
2023-02-18 11:30:31 +0530

    Upload Jenkins-main

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins! This is PES1UG20CS159\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
